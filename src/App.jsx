a554c67bc2821e4f531a591fe1e11e5c85c1d2dd
benabdallahsamir <samirbenabdallah4002@gmail.com>
2025-02-07 18:21:16 +0100

    handle state managment and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Page from "./pages/components/Page";
+import Main from "./pages/Main";
+import NewExam from "./pages/NewExam";
+import PageContainer from "./pages/components/PageContainer";
+import Quezz from "./pages/Quezz";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="w-full min-h-screen overflow-x-hidden overflow-y-auto flex">
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Page text={"page 1"} />} />
+          <Route
+            path="new"
+            element={<PageContainer children={<NewExam />} />}
+          />
+        </Route>
+        <Route path="/quize" element={<Quezz />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
